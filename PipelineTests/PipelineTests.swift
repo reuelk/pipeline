370006be50202d23a4a78db26fd4a7b01b693214
Reuel Kim <reuelkim@gmail.com>
2019-06-13 09:07:54 -0700

    Fixed missing files in Xcode project file

---
+++ PipelineTests/PipelineTests.swift
@@ -0,0 +1,34 @@
+//
+//  PipelineTests.swift
+//  PipelineTests
+//
+//  Created by Reuel Kim on 1/1/17.
+//  Copyright © 2017 Reuel Kim. All rights reserved.
+//
+
+import XCTest
+import CoreMedia
+@testable import Pipeline
+
+class PipelineTests: XCTestCase {
+	
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		
+		
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+	
+	func testPerformanceExample() {
+		// This is an example of a performance test case.
+		self.measure {
+			// Put the code you want to measure the time of here.
+		}
+	}
+	
+}
